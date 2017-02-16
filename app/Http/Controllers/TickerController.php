9bca56a5e0bcf58e0ac4ced229967115bea0e65c
Mikk Pokk <mikk@internationaleservices.eu>
2017-02-16 18:23:14 +0200

    Final version of TickerController

--- app/Http/Controllers/TickerController.php
+++ app/Http/Controllers/TickerController.php
@@ -3,13 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Rate;
-use App\Request;
 use App\Source;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 class TickerController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $active_sources = Source::where('active', true)->get();
 
@@ -32,27 +31,49 @@ class TickerController extends Controller
             $grouped_rates = Rate::whereIn('request_id', $request_ids)
                 ->whereIn('source_id', $source_ids)
                 ->get()
+                ->sortBy('currency')
                 ->groupBy('currency');
 
             $avg = [];
 
+            $output = [];
+
             foreach ($grouped_rates as $currency_key => $rates) {
                 if (! isset($avg[$currency_key])) {
-                    $avg[$currency_key] = 0;
+                    $avg[$currency_key] = [
+                        'rate' => 0,
+                        'active_sources' => 0,
+                    ];
                 }
 
-                echo $currency_key.': ';
+                $output[$currency_key] = [];
 
                 if ($rates && count($rates)) {
                     foreach ($rates as $rate) {
-                        $avg[$currency_key] += (float) $rate->rate;
+                        $avg[$currency_key]['rate'] += (float) $rate->rate;
+                        ++$avg[$currency_key]['active_sources'];
                     }
                 }
 
-                echo round($avg[$currency_key] / count($rates), 4).'<br>';
+                if ($avg[$currency_key]['rate'] > 0 && count($rates) > 0) {
+                    $rate = round($avg[$currency_key]['rate'] / count($rates), 4);
+                } else {
+                    $rate = 'Unknown';
+                }
+
+                $output[$currency_key]['active_sources'] = $avg[$currency_key]['active_sources'];
+                $output[$currency_key]['rate'] = $rate;
+                $output[$currency_key]['rate_object'] = sha1('rate_'.$currency_key);
+                $output[$currency_key]['source_object'] = sha1('source_'.$currency_key);
+            }
+
+            if ($request->ajax()) {
+                return $output;
+            } else {
+                return view('index', [
+                    'output' => $output,
+                ]);
             }
         }
-
-        //return json_decode($output, true);
     }
 }
