e726ca8d545d78be402802ff6a184f0f6fab1f19
Mikk Pokk <mikk@internationaleservices.eu>
2017-02-16 16:16:26 +0200

    Paths example added

--- updater.sh
+++ updater.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 for (( i = 0 ; i <= 59 ; i++ )); do
-    php artisan bitcoin_ticker
+    #path/to/php path/to/project/artisan bitcoin_ticker
+
+    /usr/local/Cellar/php70/7.0.12_5/bin/php /Users/mikkpokk/Documents/Sites/bitcoin-ticker/artisan bitcoin_ticker
+
     sleep 1
 done
